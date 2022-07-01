4e53c766c7b0de4c2f9a511916ffdbeccd4341b5
Sam Caslowitz <sam.caslowitz@gmail.com>
2022-07-01 14:25:28 -0700

    Updating JS.

--- plots.js
+++ plots.js
@@ -15,6 +15,20 @@ function init() {
   init();
 
   function optionChanged(newSample) {
-    console.log(newSample);
+    buildMetadata(newSample);
+    buildCharts(newSample);
   }
+
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.location);
+    });
+  }
+
   
